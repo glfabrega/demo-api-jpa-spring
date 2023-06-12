1278efe75f845ec9e9c1994c557d809b6c3d86e6
Gabriel Fabrega <glfabrega@gmail.com>
2023-06-12 17:34:17 -0300

    Project created

---
+++ src/main/java/com/glfabrega/demoapijpaspring/DemoApiJpaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.glfabrega.demoapijpaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApiJpaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApiJpaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glfabrega/demoapijpaspring/DemoApiJpaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glfabrega.demoapijpaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApiJpaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
